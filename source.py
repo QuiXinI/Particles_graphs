d86d5ff3b88599813185efb5b3bbad358192ed77
QuiXinI <130081444+QuiXinI@users.noreply.github.com>
2023-05-16 22:07:44 +0300

    stable_v0.7.2

--- source.py
+++ source.py
@@ -16,32 +16,7 @@ def check(vx, vy):
     if vy == 0:
         return 1
     else:
-        return math.degrees(math.atan(vx/vy))
-
-
-def calculate_trajectory(q, m, e, e_angle, b, v, v_angle, time_step, x, y):
-    # Вычисление векторных единиц
-    e_x = e * math.cos(math.radians(e_angle))
-    e_y = e * math.sin(math.radians(e_angle))
-    v_x = v * math.cos(math.radians(v_angle))
-    v_y = v * math.sin(math.radians(v_angle))
-
-    # Вычисление компонент ускорения
-    a_x = (q * (e_x + v_x * b)) / m
-    a_y = (q * (e_y + v_y * b)) / m
-
-    # Вычисление новых скоростей
-    v_x = v_x + a_x * time_step
-    v_y = v_y + a_y * time_step
-
-    # Вычисление новых координат
-    x += v_x * time_step
-    y += v_y * time_step
-
-    # Вычисление нового угла
-    v_angle = check(v_x, v_y)
-
-    return [x, y, v_angle]
+        return math.degrees(math.atan(vx / vy))
 
 
 # предупреждение
@@ -53,7 +28,7 @@ filler()
 
 # параметры на ввод
 q = float(input("Заряд частицы (с учётом знака, в элементарных зарядах): ")) / 6.242e+18
-m = float(input("Масса частицы (в Атомарных Единицах Массы): ")) / 6.022e+26
+m = float(input("Масса частицы (в КилоДальтонах): ")) / 6.022e+26
 v = float(input("Скорость частицы (в Метрах в Секунду): "))
 v_angle = float(input("Угол вхождения частицы в поле: "))
 b = float(input("Индукция магнитного поля (в Теслах): "))
@@ -61,17 +36,41 @@ b_direct = float(input("Направление вектора магнитной
 e = float(input("Сила электрического поля (в Ньютонах на Метр): "))
 e_angle = float(input("Угол силы электрического поля (в Градусах): "))
 time_lim = float(input("Лимит времени симуляции (в Секундах): "))
-time_step = float(input("Шаг времени симуляции (в Миллисекундах): ")) / 1000
-
-# изменение знака вектора магнитной индукции
-if b_direct == 0:
-    b *= -1
+time_step = float(input("Шаг времени симуляции (в Секундах): "))
 
 # заполнение массивов симуляции
 x = [0] * int(time_lim // time_step + 1)
 y = x
+
+# расчёт симуляции
 for i in range(1, len(x)):
-    x[i], y[i], v_angle = calculate_trajectory(q, m, e, e_angle, b, v, v_angle, time_step, x[i - 1], y[i - 1],)
+    # Вычисление векторных единиц
+    e_x = e * math.cos(math.radians(e_angle))
+    e_y = e * math.sin(math.radians(e_angle))
+    v_x = v * math.cos(math.radians(v_angle))
+    v_y = v * math.sin(math.radians(v_angle))
+
+    # Вычисление сил
+    if b_direct == 0:  # от нас
+        f_x = q * e_x + q * v_y * b
+    else:  # к нам
+        f_x = q * e_x - q * v_y * b
+    f_y = q * e_y + q * v_x * b
+
+    # Вычисление ускорений
+    a_x = f_x / m
+    a_y = f_y / m
+
+    # Вычисление новых координат
+    x[i] = x[i - 1] + v_x * time_step + a_x * (time_step ** 2) / 2
+    y[i] = y[i - 1] + v_y * time_step + a_y * (time_step ** 2) / 2
+
+    # Вычисление новых скоростей
+    v_x = v_x + a_x * time_step
+    v_y = v_y + a_y * time_step
+
+    # Вычисление нового угла
+    v_angle = check(v_x, v_y)
 
 # перевод массивов в необходимый для библиотеки тип данных
 x = np.asarray(x)
